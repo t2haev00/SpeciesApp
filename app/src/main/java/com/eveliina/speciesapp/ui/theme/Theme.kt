0ffb9d1eb75ce5265c555ead2d8b7203e7385e2d
t2haev00 <eveliinahampus@gmail.com>
2024-03-17 19:57:52 +0200

    Chore:final

--- app/src/main/java/com/eveliina/speciesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/eveliina/speciesapp/ui/theme/Theme.kt
@@ -15,26 +15,17 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green80,
+    secondary = DarkGreen80 ,
+    tertiary = LightGreen80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green40,
+    secondary = DarkGreen40,
+    tertiary = LightGreen40
 )
 
 @Composable
@@ -68,3 +59,4 @@ fun SpeciesAppTheme(
         content = content
     )
 }
+
